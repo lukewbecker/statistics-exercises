950f90edd52fee6bc58a865d0828fca387347d25
Luke Becker <luke.w.becker32@gmail.com>
2020-08-31 12:23:13 -0500

    "Added imports to practice file."

---
+++ practice_problems.py
@@ -0,0 +1,15 @@
+# Learning how to code in Python using PyCharm instead of Jupyter Notebooks
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+two_plus_four = 2+4
+two_plus_four
+
+
+simple_math = 2*2
+simple_math
+
+
